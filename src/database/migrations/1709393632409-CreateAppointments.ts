54a8e5edaca8a67b232abf84c9f2b72a77811187
erika <om.erikaorlando@gmail.com>
2024-03-02 19:11:08 +0100

    Migraciones completas

--- src/database/migrations/1709393632409-CreateAppointments.ts
+++ src/database/migrations/1709393632409-CreateAppointments.ts
@@ -1,11 +1,75 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateAppointments1709393632409 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+        await queryRunner.createTable(
+            new Table({
+            name: 'appointments',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: "increment",
+               },
+               {
+                name: "user_id",
+                type: "int",
+                //isPrimary: true,
+                isNullable: true
+             },
+             {
+                name: "dog_id",
+                type: "int",
+                //isPrimary: true,
+                isNullable: true
+             },
+             {
+                name: "activity_id",
+                type: "int",
+                //isPrimary: true,
+                isNullable: true
+             },
+              {
+                 name: "date",
+                 type: "varchar",
+                 length: "255",
+                 isNullable: true                    
+              },
+              {
+                name: "hour",
+                type: "varchar",
+                length: "255",
+                isNullable: true
+                
+             },
+             {
+                name: "is_active",
+                type: "boolean",
+                default: "true",
+             },
+             {
+              name: "created_at",
+              type: "timestamp",
+              default: "CURRENT_TIMESTAMP"
+          },
+          {
+              name: "updated_at",
+              type: "timestamp",
+              default: "CURRENT_TIMESTAMP",
+              onUpdate: "CURRENT_TIMESTAMP"
+          },
+
+],
+}),
+true
+        );
+}
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("appointments");
     }
 
 }
